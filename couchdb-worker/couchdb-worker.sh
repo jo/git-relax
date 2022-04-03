2e1611173e2a24fc1c9151245d67aac35b869aa7
Johannes Jörg Schmidt <schmidt@pm.me>
2022-04-03 13:09:29 +0200

    fix: wait for couch in couchdb worker

--- couchdb-worker/couchdb-worker.sh
+++ couchdb-worker/couchdb-worker.sh
@@ -5,8 +5,17 @@
 # - pipe changes feed to provision users
 # - pipe changes feed to provision repos
 
+COUCHDB_URL=$1
 script_path=$(dirname "$0")
 
-$script_path/global-changes-feed.sh "$1" \
-  | tee >($script_path/workers/provision-users.sh "$1") \
-  | tee >($script_path/workers/provision-repos.sh "$1" "$2")
+echo "CouchDB Worker started..."
+
+echo "waiting for CouchDB to come up"
+until $(curl --output /dev/null --silent --head --fail "$COUCHDB_URL"); do
+    printf '.'
+    sleep 1
+done
+
+$script_path/global-changes-feed.sh "$COUCHDB_URL" \
+  | tee >($script_path/workers/provision-users.sh "$COUCHDB_URL") \
+  | tee >($script_path/workers/provision-repos.sh "$COUCHDB_URL" "$2")
